7d1ac1797df254aea42478593f4608ecacb0eccc
Alejandro Salgueiro <alejandro.salgueiro@appkronos.com>
2017-02-07 16:35:16 -0300

    Update BeaconEvent.java

--- src/android/BeaconEvent.java
+++ src/android/BeaconEvent.java
@@ -51,7 +51,7 @@ public class BeaconEvent {
       response.accumulate("accion", "beacon_detect");
       
     } catch (JSONException e) {
-       response.accumulate("error", e.toString());
+       response.accumulate("error", "error al procesar (BeaconEvent)");
     }
 
     return response;
