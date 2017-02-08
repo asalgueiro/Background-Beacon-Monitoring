f9898d0f7458d75402102bdd62f663383359ade6
Alejandro Salgueiro <alejandro.salgueiro@appkronos.com>
2017-02-08 14:01:02 -0300

    Update BackgroundBeaconService.java

--- src/android/BackgroundBeaconService.java
+++ src/android/BackgroundBeaconService.java
@@ -73,12 +73,6 @@ public class BackgroundBeaconService extends Service {
   public void onDestroy() {
     Log.d("backgroundbeacons", "Destroying BackgroundBeaconService");
   }
-  
-  @Override
-  public int onStartCommand(Intent intent, int flags, int startId) {
-    Log.d("backgroundbeacons", "Service Started");
-    return START_STICKY;
-  }
 
   public IBinder onBind(Intent intent) {
     return mBinder;
